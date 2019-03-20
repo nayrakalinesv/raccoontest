54becddf499a4b899a1779bdfe300234e197b8b9
nayrakalinesv <45070701+nayrakalinesv@users.noreply.github.com>
2019-03-20 20:28:52 -0300

    FUCK YEAH its done

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 # Raccoon Test
-> teste
-<teste>
+> Projeto feito por Nayra Kaline. Feito em JavaScript, esse projeto todo foi bem interessante, do problema até a solução. Nunca tinha feito similar e nem tinha tanto conhecimento sobre as tecnologias que eu viria a utilizar. Entre elas, estão Vue para o Frontend, que eu já tinha uma pequena noção sobre e o server foi feito em express, que foi ótimo de aprender. O problema exigiu a criação de 3 funções para correção do database; correção do nome, localizada em modules/StringFunctions.js, correção dos preços e da quantidade, modules/FieldFunctions.js. Para a validação da correção do banco de dados, duas funções para exibir o resultado, em module/ProductFunctions.js. O resultado dessas funções pode ser checado na aplicação frontend, disponível na porta 8080. Espero que gostem.
 
 ## Installation
 
-Clone the project
-`git clone https://github.com/nayrakalinesv/raccoontest.git`
-Install dependencies
-`npm install`
+### Clone the project
+- `git clone https://github.com/nayrakalinesv/raccoontest.git`
+### Install dependencies
+- `npm install`
 
 ## Features
-- Fix database
-  npm run fix
+### Fix database
+-  `npm run fix`
 
-- Test database
-  npm run view
+### Test database
+- `npm run view`
 
-- Run server
-  npm run serve
+### Run server
+- `npm run serve`
 
-- Front-end application
-  npm run ui
+### Front-end application
+- `npm run ui`
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
         <div id="loading" v-if="isLoading">
             Carregando...
         </div>
+
+        <div class="lds-css ng-scope">
+        <div class="lds-blocks" style="width:100%;height:100%"><div style="left:38px;top:38px;animation-delay:0s"></div><div style="left:80px;top:38px;animation-delay:0.125s"></div><div style="left:122px;top:38px;animation-delay:0.25s"></div><div style="left:38px;top:80px;animation-delay:0.875s"></div><div style="left:122px;top:80px;animation-delay:0.375s"></div><div style="left:38px;top:122px;animation-delay:0.75s"></div><div style="left:80px;top:122px;animation-delay:0.625s"></div><div style="left:122px;top:122px;animation-delay:0.5s"></div></div>
+
     </main>
 
 </template>
@@ -47,4 +51,52 @@ export default {
         grid-gap: 1em;
         grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
     }
+
 </style>
+
+<style type="text/css">@keyframes lds-blocks {
+  0% {
+    background: #85a2b6;
+  }
+  12.5% {
+    background: #85a2b6;
+  }
+  12.625% {
+    background: #fdfdfd;
+  }
+  100% {
+    background: #fdfdfd;
+  }
+}
+@-webkit-keyframes lds-blocks {
+  0% {
+    background: #85a2b6;
+  }
+  12.5% {
+    background: #85a2b6;
+  }
+  12.625% {
+    background: #fdfdfd;
+  }
+  100% {
+    background: #fdfdfd;
+  }
+}
+.lds-blocks {
+  position: relative;
+}
+.lds-blocks div {
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background: #fdfdfd;
+  -webkit-animation: lds-blocks 1s linear infinite;
+  animation: lds-blocks 1s linear infinite;
+}
+.lds-blocks {
+  width: 200px !important;
+  height: 200px !important;
+  -webkit-transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+  transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+}
+</style></div>
