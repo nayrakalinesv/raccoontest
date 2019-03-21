257520ce4e92680665faa339d5dbfca4895c8783
nayrakalinesv <45070701+nayrakalinesv@users.noreply.github.com>
2019-03-20 21:39:36 -0300

    Alteração no carregando..

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
             Carregando...
         </div>
 
-        <div class="lds-css ng-scope">
-        <div class="lds-blocks" style="width:100%;height:100%"><div style="left:38px;top:38px;animation-delay:0s"></div><div style="left:80px;top:38px;animation-delay:0.125s"></div><div style="left:122px;top:38px;animation-delay:0.25s"></div><div style="left:38px;top:80px;animation-delay:0.875s"></div><div style="left:122px;top:80px;animation-delay:0.375s"></div><div style="left:38px;top:122px;animation-delay:0.75s"></div><div style="left:80px;top:122px;animation-delay:0.625s"></div><div style="left:122px;top:122px;animation-delay:0.5s"></div></div>
-
     </main>
 
 </template>
@@ -53,50 +50,3 @@ export default {
     }
 
 </style>
-
-<style type="text/css">@keyframes lds-blocks {
-  0% {
-    background: #85a2b6;
-  }
-  12.5% {
-    background: #85a2b6;
-  }
-  12.625% {
-    background: #fdfdfd;
-  }
-  100% {
-    background: #fdfdfd;
-  }
-}
-@-webkit-keyframes lds-blocks {
-  0% {
-    background: #85a2b6;
-  }
-  12.5% {
-    background: #85a2b6;
-  }
-  12.625% {
-    background: #fdfdfd;
-  }
-  100% {
-    background: #fdfdfd;
-  }
-}
-.lds-blocks {
-  position: relative;
-}
-.lds-blocks div {
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  background: #fdfdfd;
-  -webkit-animation: lds-blocks 1s linear infinite;
-  animation: lds-blocks 1s linear infinite;
-}
-.lds-blocks {
-  width: 200px !important;
-  height: 200px !important;
-  -webkit-transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
-  transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
-}
-</style></div>
